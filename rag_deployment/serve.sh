8eac180fd930032eb052119df2504cf0a25e01c9
davisjoseph6 <davisjoseph767@gmail.com>
2025-01-22 04:11:04 +0100

    updated

--- rag_deployment/serve.sh
+++ rag_deployment/serve.sh
@@ -1,5 +1,3 @@
-# serve.sh
-# (make sure this file is in the same folder as Dockerfile or an accessible path)
 #!/usr/bin/env bash
 echo "Starting Gunicorn (ignore extra args: $@)"
 exec gunicorn --bind 0.0.0.0:8080 app:app --timeout=120
